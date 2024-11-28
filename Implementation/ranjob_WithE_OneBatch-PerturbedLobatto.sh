5c2ef6d634c30f70c303964595c648e895774fb1
Ravi <ravipandey0706@gmail.com>
2024-11-29 03:26:22 +0530

    Commiting all the files together

---
+++ Implementation/ranjob_WithE_OneBatch-PerturbedLobatto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=S2E-Perturbed
+#SBATCH --time=04:00:00
+#SBATCH --partition=normal
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=40
+#SBATCH --account=hpc-prf-mldens
+
+
+export XLA_FLAGS="--xla_force_host_platform_device_count=100"
+srun python With_Energy/OneBatch-Perturbed_Lobatto_3A3B.py
